9c0c35cc4d001f538e50619fc10cd224a16867a2
Nepal Singh <nepal@simplyalliancess.com>
2024-03-07 20:02:14 +0530

    mail sent when offer created

--- app/models/inventory_offer.rb
+++ app/models/inventory_offer.rb
@@ -2,20 +2,25 @@ class InventoryOffer < ApplicationRecord
   belongs_to :inventory
   belongs_to :user
   delegate :user, to: :inventory, prefix: :seller, allow_nil: true
-  enum status: [ :accepted, :rejected , :sent, :resent ]
-  validates_uniqueness_of :user_id, scope: :inventory_id, message: "already sented offer"
+  enum status: { accepted: 0, rejected: 1, sent: 2, requote: 3 }
+  validates :user_id, uniqueness: { scope: :inventory_id, message: "has already sent an offer" }
+  before_validation :set_default_status, on: :create
   after_save :send_mail
 
   def send_mail
-    # if sent? || resent?
-    #   OfferMailer.offer_sent(self).deliver_now
-    # elsif accepted?
-    #   OfferMailer.offer_accepted(self).deliver_now
-    # elsif rejected?
-    #   OfferMailer.offer_rejected(self).deliver_now
-    # end
+    if sent? || requote?
+      OfferMailer.offer_sent(self).deliver_now
+      OfferMailer.offer_sent_to_admin(self).deliver_now
+    elsif accepted?
+      OfferMailer.offer_accepted(self).deliver_now
+    elsif rejected?
+      OfferMailer.offer_rejected(self).deliver_now
+    end
+  end
 
-    # OfferMailer.send("offer_#{status}", self).deliver_now
+  private
 
+  def set_default_status
+    self.status ||= :sent
   end
 end
