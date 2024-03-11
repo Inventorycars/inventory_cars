2cc75d3b48aaee585dde5c8b5e54adf83a90ceab
rorshaan <ror.shaan@gmail.com>
2024-03-11 15:25:50 +0530

    Add smtp cred in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "inventory_cars_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    domain:               'https://inventorycars.com/',
+    user_name:            'apikey',
+    password:             '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',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
